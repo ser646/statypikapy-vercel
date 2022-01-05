9236d25851137251d7f0e993abb8b8d581c6a8a0
ser646 <sebas646@gmail.com>
2022-01-05 01:55:25 +0100

    Fix

--- api/player_logs/[player_id].js
+++ api/player_logs/[player_id].js
@@ -109,10 +109,7 @@ module.exports = async (req, res) => {
                     }
                     }
                 }
-            ],
-            { 
-                allowDiskUse: true
-            },
+            ],{ allowDiskUse: true }
             ).toArray((cmdErr, result) => {
                 if(cmdErr){
                     console.log(cmdErr)
